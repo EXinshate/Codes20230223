187721279f4002e903095b7f745b20006d87149a
Nicole <758285009@qq.com>
2023-02-24 11:40:28 +0800

    20230224

---
+++ src/Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+    public static void main(String[] args){
+        byte b = 10;
+        short s = 20;
+        int i = 30;
+        long z = 92313133112565L;
+        float er = 23.24F;
+        double qwr = 21314.234;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(z);
+        System.out.println(er);
+        System.out.println(qwr);
+    }
+}
